298016a8a80a9f16071f6fa807ca3a65530dbc80
jthro <jethro.rosette@outlook.com>
2025-09-10 16:47:18 +1000

    Set up environment & gtest

---
+++ src/test.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include "test1.hpp"
+
+TEST(test_test, test_test_1) {
+    EXPECT_EQ(1, return_1());
+}
+
---
+++ src/test1.cpp
@@ -0,0 +1,5 @@
+#include "test1.hpp"
+
+int return_1() {
+    return 1;
+}
