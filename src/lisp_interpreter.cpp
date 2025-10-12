2a1420e2d07a0d04b067a9852dc702b3fba311e5
jthro <jethro.rosette@outlook.com>
2025-10-12 21:07:22 +1100

    Yes Evan; I see Them now; the Colours.

--- src/lisp_interpreter.cpp
+++ src/lisp_interpreter.cpp
@@ -1,10 +1,15 @@
 #include "lexer.hpp"
 #include <print>
+#include <ranges>
 #include <string>
+#include <string_view>
 
 auto main() -> int {
-    std::string test{"(display \"sussy baka\")"};
-    for (int i : std::ranges::iota_view(0, 5)) {
-        std::print("{}", test | lexer::lex);
-    }
+    std::string testa{"(display \"sussy baka\")\n"};
+    auto stream = std::basic_ifstream<char>{std::filesystem::path{"test.txt"}};
+    auto testb = std::ranges::istream_view<char>{stream};
+
+    std::println("{}", testa | lexer::lex<std::string_view>);
+    std::println("{}", testb | lexer::lex<std::ranges::istream_view<char>>);
+
 }
